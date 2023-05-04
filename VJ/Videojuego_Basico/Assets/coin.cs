2a4a10fa12d0f05bee449914672f933007865322
Daniel <a01782985@tec.mx>
2023-05-03 18:37:39 -0600

    Monedas .cs

---
+++ VJ/Videojuego_Basico/Assets/coin.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coin : MonoBehaviour
+{
+    public int valor = 1;
+    public GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            gameManager.SumarPuntos(valor);
+            Destroy(this.gameObject);
+        }
+    }
+}
